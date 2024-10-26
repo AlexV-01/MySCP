b66aa643e4133309ab86884fc823ed6d8550473e
Alex Varjabedian <av838@cp.cs.rutgers.edu>
2024-10-26 12:48:34 -0400

    base code

---
+++ client.h
@@ -0,0 +1,9 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+#endif
---
+++ server.c
@@ -0,0 +1,6 @@
+#include "server.h"
+
+int main(int argc, char** argv) {
+    // Setup server socket
+    return EXIT_SUCCESS;
+}
